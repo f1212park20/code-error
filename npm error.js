ac2e0970f8eaf9fe5685aa16c1de0496249ac0fb
park seung won <f1212park20@gmail.com>
2023-11-26 16:03:27 +0900

    update

--- npm error.js
+++ npm error.js
@@ -1,6 +1,6 @@
 // https://www.frontoverflow.com/question/16/npx%20create-react-app%EC%9D%B4%20%EC%9E%91%EB%8F%99%ED%95%98%EC%A7%80%20%EC%95%8A%EC%8A%B5%EB%8B%88%EB%8B%A4.
 // create-react-app 제거 후 재설치를 진행하면 된다 -> npm uninstall -g create-react-app 
 
-// npm으로 설치시 발생할수 있는 오류
-// 그냥 무시해도 상관없다
-// 8 vulnerabilities (2 moderate, 6 high)
+// https://www.inflearn.com/questions/107387/client-%EC%97%90%EC%84%9C-npm-install-%EC%9D%84%ED%95%98%EB%A9%B4-vulnerability-%EA%B0%80-%EB%A7%8E%EC%9D%B4-%EB%9C%B9%EB%8B%88%EB%8B%A4
+// npm으로 설치시 발생할수 있는 오류 -> 8 vulnerabilities (2 moderate, 6 high)
+// 해결책:그냥 무시해도 상관없다
