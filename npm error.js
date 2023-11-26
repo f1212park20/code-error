3e09bb350dbef4bb33ff826ae46b43294b1680f6
park seung won <f1212park20@gmail.com>
2023-11-26 16:02:04 +0900

    second content

---
+++ npm error.js
@@ -0,0 +1,6 @@
+// https://www.frontoverflow.com/question/16/npx%20create-react-app%EC%9D%B4%20%EC%9E%91%EB%8F%99%ED%95%98%EC%A7%80%20%EC%95%8A%EC%8A%B5%EB%8B%88%EB%8B%A4.
+// create-react-app 제거 후 재설치를 진행하면 된다 -> npm uninstall -g create-react-app 
+
+// npm으로 설치시 발생할수 있는 오류
+// 그냥 무시해도 상관없다
+// 8 vulnerabilities (2 moderate, 6 high)
